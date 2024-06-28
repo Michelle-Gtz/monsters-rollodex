05c3386e2100b367a09f25b84a36ded47feacf19
Michelle-Gtz <pmgraphics7@gmail.com>
2024-06-27 21:14:20 -0600

    fetch monsters array

--- src/App.js
+++ src/App.js
@@ -13,6 +13,20 @@ class App extends Component {
   };
 }
 
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => 
+    this.setState(
+      () => {
+        return { monsters:users };
+      },
+      () => {
+        console.log(this.state);
+      }
+    ))
+}
+
   render() {
     return (
       <div className="App">
